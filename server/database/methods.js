f37f24bb1583b8206fa53ac55584453e067eb110
vbaobao <vbao.main@gmail.com>
2021-04-14 01:17:56 -0700

    add response for no matching documents

--- server/database/methods.js
+++ server/database/methods.js
@@ -4,9 +4,11 @@ const models = require('./models.js');
 const getSimilarItemsByViews = Promise.promisify(
   (query, callback) => {
     models.similar_items_by_views.find({id: query}, (err, results) => {
-      if (err) console.error(err);
+      if (err) return console.error(err);
+      if (results.length === 0) return callback(null, 'No similar items by views found.');
       callback(null, results);
-  });
-});
+    });
+  }
+);
 
 module.exports = getSimilarItemsByViews;
