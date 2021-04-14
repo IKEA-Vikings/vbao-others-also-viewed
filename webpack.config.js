6d52ba4cff7813f74719c67cdf2012aedfc9fe67
vbaobao <vbao.main@gmail.com>
2021-04-13 20:52:37 -0700

    set up configurations

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  mode: 'development',
+  entry: './client/App.jsx',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ]
+          }
+        }
+      }
+    ]
+  }
+};
