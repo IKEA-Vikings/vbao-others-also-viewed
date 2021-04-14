9cdbf62c23e713d188978c5f9912f73db50999c6
vbaobao <vbao.main@gmail.com>
2021-04-13 22:46:48 -0700

    move methods to separate file

--- server/database/database.js
+++ server/database/database.js
@@ -1,7 +1,5 @@
 require('dotenv');
 const mongoose = require('mongoose');
-const Promise = require('bluebird');
-const models = require('./models.js');
 
 mongoose.connect(`mongodb://${process.env.DATABASE}:${process.env.DATABASE_PORT}/ikea`, {useNewUrlParser: true, useUnifiedTopology: true});
 
@@ -10,13 +8,3 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log(`The database has connected on mongodb://${process.env.DATABASE}:${process.env.DATABASE_PORT}`);
 });
-
-const getSimilarItemsByViews = Promise.promisify(
-  (query, callback) => {
-    models.similar_items_by_views.find({id: query}, (err, results) => {
-      if (err) console.error(err);
-      callback(null, results);
-  });
-});
-
-module.exports = getSimilarItemsByViews;
