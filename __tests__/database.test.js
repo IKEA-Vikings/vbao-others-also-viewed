278c1cae60acfe669a2207d334c080c87f17e15a
vbaobao <vbao.main@gmail.com>
2021-04-14 01:18:35 -0700

    debug config and write tests

--- __tests__/database.test.js
+++ __tests__/database.test.js
@@ -1,53 +1,62 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 const models = require('../server/database/models.js');
-const methods = require('../server/database/methods.js');
-
 const similar_items_by_views = models.similar_items_by_views;
+const getSimilarItemsByViews = require('../server/database/methods.js');
+const seed = require('../server/database/seed_methods.js');
 
 beforeAll(async (done) => {
-  mongoose.connect(`mongodb://${process.env.DATABASE}:${process.env.DATABASE_PORT}/test`, {useNewUrlParser: true, useUnifiedTopology: true});
+  mongoose.connect(`mongodb://${process.env.DATABASE}/test`, {useNewUrlParser: true, useUnifiedTopology: true});
 
   const db = mongoose.connection;
   db.on('error', console.error.bind(console, 'connection error:'));
   db.once('open', function() {
-    console.log(`The database has connected on mongodb://${process.env.DATABASE}:${process.env.DATABASE_PORT}`);
+    console.log(`The database has connected on mongodb://${process.env.DATABASE}`);
   });
 
   done();
 });
 
 afterAll(async (done) => {
-  similar_items_by_views.deleteMany({}, (err, results) => {
+  await similar_items_by_views.deleteMany({}, (err, results) => {
     if (err) console.error('Unable to delete the collection similar_items_by_views: ', err);
     console.log('Collection similar_items_by_views has been cleared.')
   });
+  await mongoose.disconnect();
   done();
 });
 
 describe('Seeding script', () => {
   test('Seeding script seeds 100 documents', async () => {
-    let data;
-    data = await dbMethods.getProductSizesAsync();
-    expect(data.length).toBe(100);
+    let data = seed();
+    similar_items_by_views.insertMany(data, (err, results) => {
+      if (err) return console.error('Unable to insert data. Error: ', err);
+      expect(results.length).toBe(100);
+    });
   });
 
   test('Seeds are not the same data', async () => {
-    let data;
-    data = await dbMethods.getProductSizesAsync();
-    expect(JSON.stringify(data[0])).not.toEqual(JSON.stringify(data[1]));
+    similar_items_by_views.find({id: {$in: [1, 2]}}, (err, results) => {
+      if (err) return console.error('Unable to insert data. Error: ', err);
+      expect(JSON.stringify(results[0])).not.toEqual(JSON.stringify(results[1]));
+    });
   });
 });
 
 describe('Database methods', () => {
   test('Method gets similar items by views for one item', async () => {
-    let data;
-    data = await dbMethods.getProductSizesAsync();
-    expect(data.length).toBe(100);
+    getSimilarItemsByViews(1)
+      .then((data) => {
+        expect(data.length).not.toEqual(0);
+      })
+      .catch((err) => console.error(err));
   });
 
   test('If no item is found, give proper response', async () => {
-    let data;
-    data = await dbMethods.getProductSizesAsync();
-    expect(JSON.stringify(data[0])).not.toEqual(JSON.stringify(data[1]));
+    getSimilarItemsByViews(999)
+      .then((data) => {
+        expect(data).toEqual('No similar items by views found.');
+      })
+      .catch((err) => console.error(err));
   });
 });
