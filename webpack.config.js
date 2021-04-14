2364cc96f4ce48ae60d9de4bc2e446b96dc6e040
vbaobao <vbao.main@gmail.com>
2021-04-14 01:17:00 -0700

    configure for jest

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,8 @@ module.exports = {
             presets: [
               '@babel/preset-env',
               '@babel/preset-react'
-            ]
+            ],
+            plugins: ['@babel/plugin-transform-runtime']
           }
         }
       }
