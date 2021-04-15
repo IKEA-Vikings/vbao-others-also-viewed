0fa74ccf9254c059051080003a3613f35b2afb79
vbaobao <vbao.main@gmail.com>
2021-04-14 18:42:07 -0700

    set up server endpoints

---
+++ server/server.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const cors = require('cors');
+const db = require('./database/database.js');
+const getSimilarItemsByViews = require('./database/methods.js');
+const path = require('path');
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use('/', express.static(path.resolve('public')));
+
+app.get('/:id', (req, res) => {
+  res.sendFile(path.resolve('public/index.html'));
+});
+
+app.get('/similar-products-by-views/:id', (req, res) => {
+  getSimilarItemsByViews(req.params.id)
+    .then((result) => {
+      res.rend(result);
+    })
+    .catch((err) => console.error('Unable to get similar items by views: ', err));
+});
+
+module.exports = app;
