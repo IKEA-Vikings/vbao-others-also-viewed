3cea586698912e42dcd6afd4620691eb84ae574a
vbaobao <vbao.main@gmail.com>
2021-04-15 12:42:26 -0700

    added props functionality

--- client/Carousel.jsx
+++ client/Carousel.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 import axios from 'axios';
 
 function Carousel(props) {
+  console.log('Carousel items list: ', props.items);
+  console.log('Carousel items data: ', props.data);
   return(
-    <h2>Carousel jesus</h2>
+    <div>
+      <h2>Carousel jesus</h2>
+      <div>Items: {props.items}</div>
+      <div>Data: {JSON.stringify(props.data)}</div>
+    </div>
   );
 }
 
