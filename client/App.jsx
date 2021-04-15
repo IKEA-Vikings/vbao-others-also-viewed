76a7396f9aa5b50fe9664906964e0e7cd5d6feb1
vbaobao <vbao.main@gmail.com>
2021-04-14 19:15:23 -0700

    set up main render

---
+++ client/App.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import OthersAlsoViewed from './OthersAlsoViewed.jsx';
+
+ReactDOM.render(<OthersAlsoViewed />, document.getElementById('others-also-viewed'));
