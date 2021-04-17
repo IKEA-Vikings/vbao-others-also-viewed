9214291d443d981de119255538312553674d04b2
vbaobao <vbao.main@gmail.com>
2021-04-16 19:02:56 -0700

    add configs for hooks and icons

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,10 @@ module.exports = {
               '@babel/preset-env',
               '@babel/preset-react'
             ],
-            plugins: ['@babel/plugin-transform-runtime']
+            plugins: [
+              '@babel/plugin-transform-runtime',
+              '@babel/plugin-proposal-class-properties'
+          ]
           }
         }
       }
